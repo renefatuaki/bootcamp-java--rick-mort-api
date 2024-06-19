9abd68217dd0a08ff448775007d255992039ba36
René El Fatuaki <rene@elfatuaki.com>
2024-06-19 10:46:48 +0200

    Init

---
+++ src/main/java/dev/elfa/rickmortyapi/BootcampJavaRickMortyApiApplication.java
@@ -0,0 +1,13 @@
+package dev.elfa.rickmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJavaRickMortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampJavaRickMortyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elfa/rickmortyapi/BootcampJavaRickMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elfa.rickmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampJavaRickMortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
