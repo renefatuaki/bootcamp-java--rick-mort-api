4c7eb71d0e7eab70975ace4bb753f188df1347be
René El Fatuaki <rene@elfatuaki.com>
2024-06-19 17:09:56 +0200

    Mocking tests for filtering by status

--- src/test/java/dev/elfa/rickmortyapi/controller/RickMortyControllerTest.java
+++ src/test/java/dev/elfa/rickmortyapi/controller/RickMortyControllerTest.java
@@ -163,4 +163,57 @@ class RickMortyControllerTest {
                         ]
                         """));
     }
+
+    @Test
+    void getCharactersByStatus() throws Exception {
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader("Content-Type", "application/json")
+                .setBody("""
+                        {
+                            "info": {
+                                "count": 826,
+                                "pages": 42,
+                                "next": "https://rickandmortyapi.com/api/character?page=2",
+                                "prev": null
+                            },
+                            "results": [
+                                {
+                                    "id": 1,
+                                    "name": "Rick Sanchez",
+                                    "status": "Alive",
+                                    "species": "Human",
+                                    "type": "",
+                                    "gender": "Male",
+                                    "origin": {
+                                        "name": "Earth (C-137)",
+                                        "url": "https://rickandmortyapi.com/api/location/1"
+                                    },
+                                    "location": {
+                                        "name": "Citadel of Ricks",
+                                        "url": "https://rickandmortyapi.com/api/location/3"
+                                    },
+                                    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                                    "episode": [
+                                        "https://rickandmortyapi.com/api/episode/1"
+                                    ],
+                                    "url": "https://rickandmortyapi.com/api/character/1",
+                                    "created": "2017-11-04T18:48:46.250Z"
+                                }
+                            ]
+                        }
+                        """)
+        );
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/characters?status=alive"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        [
+                            {
+                                "id": "1",
+                                "name": "Rick Sanchez",
+                                "species": "Human"
+                            }
+                        ]
+                        """));
+    }
 }
